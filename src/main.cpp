67f2719220d7a0833f3653055deb55523664e5c2
Hamilton Little <hamilton.little@gmail.com>
2016-06-20 18:31:29 -0700

    Printing binary image data to console.

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,53 @@ int main(int argc, char *argv[])
 
     // open and load the binary image
     std::string fileName = argv[1];
-    std::ifstream raw_image(fileName, std::ios::in);
-    if(!raw_image || raw_image.bad()) {
+    std::ifstream image_file(fileName, std::ios::in | std::ios::binary);
+    if(!image_file || image_file.bad()) {
         fail(progName, std::string("unable to open image file: ") + argv[1]);
     }
 
-    std::cout << "Hello World" << std::endl;
+    // get the raw data
+    constexpr int IMAGE_SIDE_LEN = 256;
+    constexpr int PIXELS_PER_BYTE = 8;
+    constexpr size_t RAW_IMAGE_SIZE = IMAGE_SIDE_LEN * IMAGE_SIDE_LEN / PIXELS_PER_BYTE;
+
+    std::vector<bool> data; // use false for black, true for white
+    data.reserve(RAW_IMAGE_SIZE);
+
+    image_file.seekg(0, std::ios::end);
+    size_t file_size = image_file.tellg();
+
+    // there is probably a better way to do this
+    size_t header_size = file_size - RAW_IMAGE_SIZE;
+    image_file.seekg(header_size, std::ios::beg); // skip over the header
+    size_t bytesLeft = RAW_IMAGE_SIZE;
+    while(bytesLeft--) {
+        using Byte = char;
+        Byte buffer[1];
+        image_file.read(buffer, 1);
+
+        Byte& nextByte = buffer[0];
+        for (int ndx = 0; ndx < PIXELS_PER_BYTE; ++ndx) {
+            constexpr uint8_t mask = 0b1000000;
+            Byte masked_value = ((mask >> ndx) & nextByte);
+            bool is_set = (masked_value != 0);
+            data.push_back(is_set);
+        }
+
+        if(image_file.fail()) {
+            fail(progName, "Error parsing binary image data");
+        }
+    }
+
+    int ndx = 0;
+    for(auto bit : data) {
+        if(ndx++ % IMAGE_SIDE_LEN == 0) {
+            std::cout << std::endl;
+        }
+
+        std::cout << (bit ? "x" : "_");
+    }
+
+    std::cout << std::endl;
     return 0;
 }
